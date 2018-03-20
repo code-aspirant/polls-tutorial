695e70a09ed249d651a2646f034196efe439e406
code_aspirant <concerningasteroids@gmail.com>
2018-03-19 23:52:27 -0400

    start adding requests and responses for polls

---
+++ src/main/java/com/zagsoft/polls/payload/UserSummary.java
@@ -0,0 +1,12 @@
+package com.zagsoft.polls.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserSummary {
+    private Long id;
+    private String username;
+    private String name;
+}
